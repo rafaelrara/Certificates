1479f8cb6b592af1cea9a60a9642efbcb6209d1d
Rafael A.R.A <rafael_rar@hotmail.com>
2022-01-20 16:18:48 -0300

    add js clock

---
+++ script.js
@@ -0,0 +1,22 @@
+let txt;
+
+function showTimer(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    m = zero(m);
+    s = zero(s);
+
+    txt = h + ":" + m + ":" + s;
+    document.querySelector(".timer").innerHTML= txt;
+}
+
+function zero(x){
+    if (x < 10){
+        x = '0' + x;
+    }return x;
+}
+
+let timer = setInterval(showTimer, 1000);
